f16371d80cb96202cd7d71eddcf6ff324e7eba84
Sebastian Fudulu <sebastian.fudulu05@e-uvt.ro>
2025-11-06 13:59:36 +0200

    reupload pictures and fixing pictures not showing

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def update_product(product_id):
         values.append(current_price)
     if ad_value:
         fields.append("ad_value = %s")
-        values.append(ad_value)
         fields.append("current_price = current_price - %s")
+        values.append(ad_value)
         values.append(ad_value)
     if active_until:
         fields.append("active_until = %s")
